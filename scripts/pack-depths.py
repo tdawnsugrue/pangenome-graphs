dcfdf79384d195a5effce1f27e771b6c5b815458
Toby Sugrue <fien.joaas@gmail.com>
2024-05-14 14:49:28 +1200

    graphing coverage depth

---
+++ scripts/pack-depths.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+
+coverages = pd.read_csv("data/packed-fcgr-depth-thing.tsv", delimiter="\t", header=None)
+coverages = coverages.rename(columns={0 : 'path-maybe', 1 : 'position', 2 : 'coverage'})
+
+grch = "GRCh38#chr1:161505240-161678751"
+
+coverages = coverages[coverages['path-maybe']==grch]
+print(coverages.tail())
+subsetted = coverages[coverages['position'] > 161623196 -161505240 - 20000]
+print("start pos", 161623196 -161505240)
+
+sns.lineplot(x="position", y="coverage", data=subsetted)
+plt.show()
