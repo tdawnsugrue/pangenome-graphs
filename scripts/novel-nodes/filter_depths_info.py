a1cd839ae9eebfa2bd6354339951db5b10ad6272
tdawnsugrue <61959565+tdawnsugrue@users.noreply.github.com>
2024-09-25 12:05:23 +1200

    all chroms filter depth

--- scripts/novel-nodes/filter_depths_info.py
+++ scripts/novel-nodes/filter_depths_info.py
@@ -2,33 +2,51 @@ import sys
 
 # Filters a depth file based on the total number of haplotypes
 
-if len(sys.argv) < 3 or len(sys.argv) > 5:
-    print("ERROR: invalid arguments.\nUsage: python filter_depths_info.py [DEPTH_FILE] [NODE_FILE]", file=sys.stderr)
+chrom_dir = "nonref_files/"
 
-nodes = [line.split() for line in open(sys.argv[2]).read().split('\n')][:-1]
-depths = [line.split() for line in open(sys.argv[1]).read().split('\n')][:-1] # nodes are 1-indexed so leave the header in
+# if len(sys.argv) < 3 or len(sys.argv) > 5:
+#     print("ERROR: invalid arguments.\nUsage: python filter_depths_info.py [DEPTH_FILE] [NODE_FILE]", file=sys.stderr)
 
-depth_idx = [int(d[0]) for d in depths[1:]]
+# SO in nonref_files we have chr[xyz]-full-node-depth.txt and chr[xyz]-novel-nodes-1k.txt
+chromosomes = [i for i in range(1, 23)]
+exclude = [5]
 
-min_haps = sys.argv[3] if len(sys.argv) > 3 else 10
-max_haps = sys.argv[4] if len(sys.argv) > 4 else 50
+for chrom in chromosomes:
+    if chrom in exclude : continue
 
-missing_nodes = 0 
-# print depths for nodes in filtered nodes file
-for node in nodes:
-    n = int(node[0])
-    if n > int(depths[-1][0]):
-        print("Node greater than last node in 'full' depth list.", file=sys.stderr)
-        break
-    
-    try:
-        d = depths[depth_idx.index(n)+1]
-    except:
-        missing_nodes += 1
-        continue
-    depth = int(d[1])
-    if depth >= min_haps and depth <= max_haps :
-        print(node, d, file=sys.stderr)
-        print(f"{d[0]}\t{d[1]}\t{d[2]}")
+    print("Getting nodes for chrom", chrom, file=sys.stderr)
 
-print(missing_nodes, "missing nodes", file=sys.stderr)
+    nodes = [line.split() for line in open(f"{chrom_dir}chr{chrom}-novel-nodes-1k.txt").read().split('\n')][:-1]
+    depths = [line.split() for line in open(f"{chrom_dir}chr{chrom}-full-node-depth.txt").read().split('\n')][:-1] # nodes are 1-indexed so leave the header in
+
+    depth_idx = [int(d[0]) for d in depths[1:]]
+
+    min_haps = sys.argv[3] if len(sys.argv) > 3 else 10
+    max_haps = sys.argv[4] if len(sys.argv) > 4 else 50
+
+    f = open(f"{chrom_dir}chr{chrom}-filt-1k-{min_haps}-{max_haps}.txt", "w")
+
+    to_write = ""
+    missing_nodes = 0 
+    # print depths for nodes in filtered nodes file
+    for node in nodes:
+        n = int(node[0])
+        if n > int(depths[-1][0]):
+            print("Node greater than last node in 'full' depth list.", file=sys.stderr)
+            break
+        
+        try:
+            d = depths[depth_idx.index(n)+1]
+        except:
+            missing_nodes += 1
+            continue
+        depth = int(d[1])
+        if depth >= min_haps and depth <= max_haps :
+            print(node, d, file=sys.stderr)
+            to_write += f"{d[0]}\t{d[1]}\t{d[2]}\n"
+
+    f.write(to_write)
+    f.close()
+
+
+    print(missing_nodes, "missing nodes", file=sys.stderr)
