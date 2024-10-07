abc2c2bb3561fbb21ab80f4f604a6cc197a1134f
tdawnsugrue <61959565+tdawnsugrue@users.noreply.github.com>
2024-10-08 12:03:00 +1300

    timing random graphs

--- scripts/speed-testing/rand_graph_stats.py
+++ scripts/speed-testing/rand_graph_stats.py
@@ -7,17 +7,40 @@
 # write em all to a tsv
 import subprocess
 import os
+from sys import stderr
 
 graph_dir = "graphs/random/"
 
 graphs = os.listdir(graph_dir)
 
-for graph in graphs[:1]:
+all_stats = []
+
+for graph in graphs:
     # odgi paths to get first path name
     # run time on the bash script < args FILE_PATH GRAPH_PATH
     # get sys time
     # store graphID TIME
+    length = graph[graph.find("-")+1:graph.rfind("-")]
+    length = length[length.find("-")+1:]
+    
     path = subprocess.run(["odgi", "paths", "-i", graph_dir + graph, "-L"], capture_output=True).stdout.decode()
-    path = path.split("\n")
-    print(path)
-    pass
+    path = path.split("\n")[0]
+    
+    time = subprocess.run(["time", "-p", "./scripts/time_rand_pipe.sh", graph_dir + graph, path], 
+                          capture_output=True).stderr.decode().split("\n")[1:-1]
+
+    a = 0
+    for t in time:
+        a += float(t[t.find(" ")+1:])
+    time = f"{a:.3f}"
+
+    stats = subprocess.run(["odgi", "stats", "-i", graph_dir + graph, "-S"], capture_output=True).stdout.decode().split("\n")
+    stats = stats[1].split("\t")[:2]
+
+    all_stats.append("\t".join([graph, length, *stats, time]))
+    print(f"testing {graph}", file=stderr)
+
+with open("data/speed_stats.tsv", "w") as file:
+    file.write("\n".join(all_stats) + "\n")
+
+print("done", file=stderr)
--- scripts/time_rand_pipe.sh
+++ scripts/time_rand_pipe.sh
@@ -1,5 +1,5 @@
 
 
-odgi extract -i $1 -o tmp.og -E -t16 -r "$2:10-10000"
+odgi extract -i $1 -o tmp.og -E -r "$2:10-10000" -t16
 odgi sort -i $1 -o tmp.og -L -t16
 odgi viz -i tmp.og -o tmp.png -s "#" -t16
