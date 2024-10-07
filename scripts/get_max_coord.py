b91598a9a423cfff61c7b3973391e5b07735488b
tdawnsugrue <61959565+tdawnsugrue@users.noreply.github.com>
2024-10-07 15:17:07 +1300

    fix err

--- scripts/get_max_coord.py
+++ scripts/get_max_coord.py
@@ -7,11 +7,12 @@
 import os
 import subprocess
 
-chrom_graphs = os.listdir("graphs/chroms")
+cdir = "graphs/chroms"
+chrom_graphs = os.listdir(cdir)
 
 # 1
 
 for i in range(len(chrom_graphs))[:1]:
-    stats = subprocess.run(["odgi", "stats", "-i", chrom_graphs[i], "-S", "-t8"], capture_output=True).stdout.decode().split("\n")[1]
+    stats = subprocess.run(["odgi", "stats", "-i", cdir + "/" + chrom_graphs[i], "-S", "-t8"], capture_output=True).stdout.decode().split("\n")
     print(stats)
     pass
