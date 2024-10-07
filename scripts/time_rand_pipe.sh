ce6746fb3eda1291a4c6e5290851e3e6826cc93e
tdawnsugrue <61959565+tdawnsugrue@users.noreply.github.com>
2024-10-07 21:52:27 +1300

    timing random graphs

--- scripts/time_rand_pipe.sh
+++ scripts/time_rand_pipe.sh
@@ -5,5 +5,7 @@ touch data/rand_times_raw.txt
 
 for file in ./graphs/random/*
 do
+    echo "doing ${file}"
     echo "${file}" >> data/rand_times_raw.txt
-    time <(odgi extract -i $file -o _ -E -t16 && odgi sort -i $file -L && odgi viz -i ) >> data/rand_times_raw.txt
+    time <(odgi extract -i $file -o _ -E -t16 && odgi sort -i $file -L &&
+            odgi viz -i $file -o _ -s '#') >> data/rand_times_raw.txt
