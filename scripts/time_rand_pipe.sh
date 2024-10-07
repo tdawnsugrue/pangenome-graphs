4f3785ea9bb6519de1b0d832c4abe7601bd6c5f8
tdawnsugrue <fien.joaas@gmail.com>
2024-10-08 11:23:37 +1300

    fixed bug

--- scripts/time_rand_pipe.sh
+++ scripts/time_rand_pipe.sh
@@ -1,3 +1,5 @@
-odgi extract -i $1 -o tmp.og -E -t16 -r "GRCh38#chr$2:10-10000"
-odgi sort -i $1 -o tmp.og -L
-odgi viz -i tmp.og -o tmp.png -s "#"
+
+
+odgi extract -i $1 -o tmp.og -E -t16 -r "$2:10-10000"
+odgi sort -i $1 -o tmp.og -L -t16
+odgi viz -i tmp.og -o tmp.png -s "#" -t16
